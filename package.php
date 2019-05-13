50746c63106bf283102a4c2384b01f0fe227002d
Matt Allan <mattallan@fastmail.com>
2019-05-13 13:30:50 -0400

    slightly better package script

--- package.php
+++ package.php
@@ -1,15 +1,25 @@
 <?php
 
+$filename = $argv[1] ?? false;
+
+if (!$filename || !file_exists($filename)) {
+    echo 'Usage: php package.php [filename]';
+    exit(0);
+}
+
 $src = fopen(__DIR__ . '/build/embed', 'r');
 $binary = fread($src, filesize(__DIR__ . '/build/embed'));
 fclose($src);
 
-$src = fopen(__DIR__ . '/code.php', 'r');
-$code = fread($src, $codeLength = filesize(__DIR__ . '/code.php'));
+$src = fopen($filename, 'r');
+$code = fread($src, $codeLength = filesize($filename));
 fclose($src);
 
-$dest = fopen(__DIR__ . '/build/greeter', 'w');
+$dest = fopen(__DIR__ . '/build/' . basename($filename, '.php'), 'w');
 fwrite($dest, $binary);
 fwrite($dest, $code);
 fwrite($dest, pack('V', $codeLength));
 fclose($dest);
+
+echo "Packaging complete.\n";
+exit(1);
